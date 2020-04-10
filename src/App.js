821951a28b6082e262e24b466cc5fb5db779ab7d
Miguel Cobas <mcobas@dealergeek.com>
2020-04-10 00:18:41 -0500

    Se creo la funcion de consultar al API usando fetch, async y await

--- src/App.js
+++ src/App.js
@@ -25,8 +25,10 @@ const Boton = styled.button`
 
 function App() {
 
-  const consultarApi = () =>{
-    console.log("consultando")
+  const consultarApi = async () =>{
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json()
+     console.log(frase[0])
   }
 
   return (
